3a5bdd397a2039744914d922b3ba88400bf5f1d4
Sheng Liu <67348655+shengliu66@users.noreply.github.com>
2022-08-19 15:52:35 -0400

    Delete run_cifar100.sh

--- run_cifar100.sh
+++
@@ -1,1 +0,0 @@
-bsub -q normal -gpu "num=1:mode=shared:j_exclusive=yes" -J "Overparam_$noise" -n 2 -R "rusage[mem=8]" -oo OverPara_cifar100.o%J -eo OverPara_cifar100.e%J python train.py -c config_cifar100.json --lr_u 1 --lr_v 100 --percent 0.4 --name CIFAR100 --asym True
